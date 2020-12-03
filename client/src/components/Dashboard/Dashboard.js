6b168ba6c6cec4b42c94f52de1a7b583733d6bc9
Baptiste Desarnauts <bat.desarnauts@gmail.com>
2020-12-03 16:38:16 +0100

    bug fix

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
-import Axios from 'axios'
-import "./Dashboard.css"
+import Axios from "axios";
+import "./Dashboard.css";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props){
-        super(props);
-    };
+  fetchdata() {}
 
-    fetchdata(){}
+  render() {
+    return (
+      <div className="container">
+        <h1>Tableau de bord</h1>
+        {/* <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMjg3NDVmZjYtYmQyOC00MmNlLWFkZDktNTNjNWVhOWQ5ZGY2IiwidCI6ImI3M2MzZGU3LTcxYmMtNGRkNC04ZDlmLWNlMTRlMTY5NDA3OCJ9" frameborder="0" allowFullScreen="true"></iframe> */}
+        <iframe
+          width="1024"
+          height="804"
+          src="https://app.powerbi.com/view?r=eyJrIjoiMjg3NDVmZjYtYmQyOC00MmNlLWFkZDktNTNjNWVhOWQ5ZGY2IiwidCI6ImI3M2MzZGU3LTcxYmMtNGRkNC04ZDlmLWNlMTRlMTY5NDA3OCJ9&pageName=ReportSection29fa2e7c3180e0a11da0"
+          frameborder="0"
+          allowFullScreen="true"
+        ></iframe>
+      </div>
+    );
+  }
+}
 
-    render(){
-        return(
-            <div class = "main">
-                <h1>Dashboard</h1>
-                {/* <iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMjg3NDVmZjYtYmQyOC00MmNlLWFkZDktNTNjNWVhOWQ5ZGY2IiwidCI6ImI3M2MzZGU3LTcxYmMtNGRkNC04ZDlmLWNlMTRlMTY5NDA3OCJ9" frameborder="0" allowFullScreen="true"></iframe> */}
-                <iframe width="800" height="636" src="https://app.powerbi.com/view?r=eyJrIjoiMjg3NDVmZjYtYmQyOC00MmNlLWFkZDktNTNjNWVhOWQ5ZGY2IiwidCI6ImI3M2MzZGU3LTcxYmMtNGRkNC04ZDlmLWNlMTRlMTY5NDA3OCJ9&pageName=ReportSection29fa2e7c3180e0a11da0" frameborder="0" allowFullScreen="true"></iframe>
-            </div>
-        
-
-        )
-    }
-};
-
-export default Dashboard
+export default Dashboard;
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import { push as Menu } from 'react-burger-menu'
-import "./Sidebar.css"
+import { push as Menu } from "react-burger-menu";
+import "./Sidebar.css";
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props){
-        super(props);
-    };
-
-    render() {
-
-        return(
-            <Menu>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="add_data" className="menu-item" href="/adddata">Add Data</a>
-                <a id="data_manager" className="menu-item" href="/datamanager">Manage Data</a>
-            </Menu>
-        );
-    }
+  render() {
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">
+          Tableau de bord
+        </a>
+        <a id="add_data" className="menu-item" href="/adddata">
+          Ajout de données
+        </a>
+        <a id="data_manager" className="menu-item" href="/datamanager">
+          Gérer les données
+        </a>
+      </Menu>
+    );
+  }
 }
-export default Sidebar
+export default Sidebar;
